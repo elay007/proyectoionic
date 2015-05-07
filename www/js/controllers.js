9aec52c44de0b21a0d56e01d2f756908d5bf156a
Alvaro Molina <alvaromolinac@gmail.com>
2015-05-06 22:13:36 -0400

    init

---
+++ www/js/controllers.js
@@ -0,0 +1,31 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope) {
+	$scope.products = [{'name':'Iphone', 'prices': 78.10, 'img':'http://www.att.com/wireless/iphone/assets/207138-iPhone6-device2.jpg'}, {'name':'Samsung', 'prices': 78.10, 'img': 'http://www.att.com/wireless/iphone/assets/207138-iPhone6-device2.jpg'}] 
+})
+
+.controller('ChatsCtrl', function($scope, Chats) {
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  }
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('SignInCtrl', function($scope) {
+
+})
+
+.controller('SignUpCtrl', function($scope) {
+
+})
+
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+});
